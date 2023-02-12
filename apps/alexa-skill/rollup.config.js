0ab82b095d698a66f074da1af06db1403d51ab52
John Sorial <john.sorial@pickware.de>
2023-02-12 15:23:46 +0200

    Welcome back yarn v2

--- apps/alexa-skill/rollup.config.js
+++ apps/alexa-skill/rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import {defineConfig} from 'rollup';
 import {sentryRollupPlugin} from "@sentry/rollup-plugin";
+import copy from "rollup-plugin-copy";
 
 export default defineConfig({
     input: ['src/index.ts'],
@@ -17,14 +18,28 @@ export default defineConfig({
         nodeResolve({browser: false, preferBuiltins: true,}),
         commonjs(),
         json({compact: true, preferConst: true, namedExports: true}),
+        copy({
+            targets: [
+                {src: "prisma/schema.prisma", dest: "dist"},
+                {
+                    src: "../../node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node",
+                    dest: "dist"
+                },
+                {src: "package.json", dest: "dist"},
+                {src: "../../node_modules/hafas-client/p/db/base.json", dest: "dist"},
+
+            ]
+        }),
         sentryRollupPlugin({
             org: "myself-2o",
             project: "alexa-bahn",
             // Specify the directory containing build artifacts
-            include: "./dist",
+            include: [{paths: ["./dist"], ext: [".js", ".cjs", ".map"]}, {paths: ["./src"], ext: [".ts"]}],
             ignore: ["node_modules"],
-
+            urlPrefix: "app:///../../src",
+            stripCommonPrefix: true,
             sourceMapReference: true,
+            cleanArtifacts:true,
             validate: true,
             rewrite: true
         }),
